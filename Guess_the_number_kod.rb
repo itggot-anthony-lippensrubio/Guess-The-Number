8f74bf48944cec4ff0beea8f61c0288493f1df8f
anthony lippens rubio <anthony.lippensrubio@itggot.se>
2017-03-30 11:32:29 +0200

    Kod snart klart
    Finslippar koden

--- Guess_the_number_kod.rb
+++ Guess_the_number_kod.rb
@@ -1,52 +1,62 @@
 
 def welcome
+  system('cls')
   puts 'Welcome to "Guess the Secret Number"'
-  sleep 2
+  sleep 1.5
   system('cls')
-  sleep 0.2
+  sleep 0.4
   puts "Select Difficulty!"
-  sleep 2
+  sleep 1.5
   system('cls')
-  sleep 0.2
+  sleep 0.4
   puts "Diffuclty 1-4"
-  input1 = gets.to_i
-  return input1
+  input = gets.to_i
+  system('cls')
+  return input
 end
 
 def randome_number
-  input = input1
+  input = welcome
   ran_num = 0
-  if input == 1
-    ran_num = rand(1..10)
-    return ran_num
-  elsif input == 2
-    ran_num = rand(1..20)
-    return ran_num
-  elsif input == 3
-    ran_num = rand(1..50)
-    return ran_num
-  else input == 4
-    ran_num = rand(1..100)
-    return ran_num
-  end
-end
 
-def guess_the_number
-  secret_number = ran_num
-  running = true
-  while running
-    input2 = gets.to_i
-    if input2 == secret_number
-      puts "Correct! The secret number was" + secret_number
-      running = false
-    elsif input2 == 'quit'
-      running = false
+    if input == 1
+    ran_num = rand(1..10)
+      return ran_num
     end
   end
 end
 
+def guess_the_number
+  secret_number = randome_number
+  running = true
+  tries = 0
+  while running
+    puts "Guess the secret number"
+    puts "The secret number is between 1-10"
+    input2 = gets.to_i
 
+    if input2 == secret_number
+      puts "Correct! The secret number was #{secret_number}."
+      tries += 1
+      puts "Congratilations! You got it in #{tries} tries!"
+      sleep 3
+      system('cls')
+      running = false
 
+    elsif input2 < secret_number
+      puts "Wrong the secret number is higher."
+      tries += 1
+      sleep 1
+      system('cls')
 
-puts welcome
+    else
+      puts "Wrong the secret number is lower."
+      tries += 1
+      sleep 1
+      system('cls')
+
+    end
+  end
+end
+
 puts guess_the_number
